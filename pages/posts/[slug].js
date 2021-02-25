6c92bddd68c015a78b2dfcb5d43e7b7da834b38d
samuelghill <samuelgarretthill@gmail.com>
2021-02-25 16:01:51 -0600

    working demo.  Need some more on the post page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -7,77 +7,70 @@ import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
 import SectionSeparator from '../../components/section-separator'
 import Layout from '../../components/layout'
-import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
+import { getAllPostsWithSlug, getPostAndMorePosts, getWPPost } from '../../lib/api'
 import PostTitle from '../../components/post-title'
 import Head from 'next/head'
 import { CMS_NAME } from '../../lib/constants'
 import Tags from '../../components/tags'
 
-export default function Post({ post, posts, preview }) {
+export default function Post({ wordpressPost, preview }) {
   const router = useRouter()
-  const morePosts = posts?.edges
+  // const morePosts = posts?.edges
 
-  if (!router.isFallback && !post?.slug) {
+  if (!router.isFallback && !wordpressPost?.slug) {
     return <ErrorPage statusCode={404} />
   }
 
   return (
-    <Layout preview={preview}>
-      <Container>
-        <Header />
-        {router.isFallback ? (
+    <>
+      <Layout preview={preview}>
+          {router.isFallback ? (
           <PostTitle>Loading…</PostTitle>
         ) : (
           <>
             <article>
               <Head>
                 <title>
-                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                  {wordpressPost.title} | Next.js Blog Example with {CMS_NAME}
                 </title>
                 <meta
                   property="og:image"
-                  content={post.featuredImage?.node?.sourceUrl}
+                  content={wordpressPost.featuredImage}
                 />
               </Head>
               <PostHeader
-                title={post.title}
-                coverImage={post.featuredImage?.node}
-                date={post.date}
-                author={post.author?.node}
-                categories={post.categories}
+                title={wordpressPost.title}
+                coverImage={wordpressPost.featuredImage?.node}
+                date="12-22-2020"
+                author="sam"
+                categories="bicycle"
               />
-              <PostBody content={post.content} />
-              <footer>
+              <PostBody content={wordpressPost.content} />
+              {/* <footer>
                 {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
-              </footer>
+              </footer> */}
             </article>
 
             <SectionSeparator />
-            {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+            {/* {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
           </>
         )}
-      </Container>
-    </Layout>
+      </Layout>
+    </>
   )
 }
 
-export async function getStaticProps({ params, preview = false, previewData }) {
-  const data = await getPostAndMorePosts(params.slug, preview, previewData)
-
+export async function getStaticProps({ preview = false, id }) {
+  const wordpressPost = await getWPPost(preview, id = 1)
   return {
-    props: {
-      preview,
-      post: data.post,
-      posts: data.posts,
-    },
+    props: { wordpressPost, preview, id },
   }
 }
 
 export async function getStaticPaths() {
-  const allPosts = await getAllPostsWithSlug()
-
+  const wordpressPosts = await getAllPostsWithSlug()
   return {
-    paths: allPosts.edges.map(({ node }) => `/posts/${node.slug}`) || [],
+    paths: wordpressPosts.map((node) => `/posts/${node.slug}`) || [],
     fallback: true,
   }
 }
